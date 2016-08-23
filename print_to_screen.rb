829904fb962c05ef37cd6fe699197f520f78990c
Susan Graves <susangraves@Susans-MacBook-Air.local>
2016-08-23 11:16:52 -0400

    First attempt replacing 3 and 5

---
+++ print_to_screen.rb
@@ -0,0 +1,50 @@
+# puts "This is a string that we want to print to the screen " * 100
+
+# puts "This is a string that we want to print to the screen
+# " * 100
+
+# 100.times do 
+# 	puts This is a string that we want to print to the screen
+	
+# end
+
+# 100.times do
+# 	puts "1\n"
+# end
+x = 1
+	while x <= 100
+		if x == 3
+			puts "Mined"
+			if x == 5
+				puts "Minds"
+			else
+				puts x
+				puts ""
+			end
+		
+		else
+			if x == 5
+				puts "Minds"
+			else
+
+		puts x
+		puts ""
+			end
+
+		end
+
+		
+
+		x = x + 1
+	end
+
+
+# print 1.step(100).to_a
+# puts 1.step(100).to_a
+
+# x = 1
+# 100.times do
+# 	puts "The number is #{1.step(100).to_a}"
+# end
+
+# (1..100).each {|n| puts n}
